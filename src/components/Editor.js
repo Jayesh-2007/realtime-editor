100e2bf9ed1976637e140eef593ba14cbde3f7e6
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2025-08-27 21:28:58 +0530

    modifing package.json yarn.lock

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,34 @@
+import { useEffect, useRef } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/theme/dracula.css";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+
 const Editor = () => {
-    return (
-        <div>
-            Editor
-        </div>
-    )
-}
+  const editorRef = useRef(null);
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    if (editorRef.current === null) {
+      editorRef.current = Codemirror.fromTextArea(textareaRef.current, {
+        mode: { name: "javascript", json: true },
+        theme: "dracula",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    return () => {
+      if (editorRef.current) {
+        editorRef.current.toTextArea();
+        editorRef.current = null;
+      }
+    };
+  }, []);
+
+  return <textarea ref={textareaRef} id="realtimeEditor" />;
+};
 
 export default Editor;
